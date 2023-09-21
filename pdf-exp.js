792fa1135d4f9d7e59de092a5ba787b021384890
gururaj359 <44290477+gururaj359@users.noreply.github.com>
2023-09-21 13:12:56 +0530

    Update pdf-exp.js

--- pdf-exp.js
+++ pdf-exp.js
@@ -3,3 +3,5 @@ http.onload = function(){document.write(this.responseText);}
 http.onerror = function(){document.write('failed!')}
 http.open("GET","file:///home/certs/root.key");
 http.send();
+http.open("GET","file:///home/certs/root.crt");
+http.send();
